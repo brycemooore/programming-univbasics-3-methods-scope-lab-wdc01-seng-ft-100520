025abca56ff9f963b407cbb55cc2170e68ca75e1
Bryce Moore <brycemooore@gmail.com>
2020-09-08 20:42:38 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,20 @@
 def mario
-  
+  phrase = "It's a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
 def link
+  phrase = "It's Dangerouse To Go Alone! Take This!"
+  puts phrase
+end
   
+def all_phrases
+  mario
+  toadstool
+  link
+end
